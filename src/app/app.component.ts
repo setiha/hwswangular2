19d1023c431efd84324a5bdc7bb37d7aeada84a4
Tihici <tihamer.sebok@gmail.com>
2020-07-23 11:23:35 +0300

    strukturalis es attributum direktivak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Fruit} from "./fruit";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularproject2';
+  fruits: Fruit[] =
+    [
+      {name: 'watermelon', color: 'pink', score: 10},
+      {name: 'grape', color: 'green', score: 7},
+      {name: 'cantaloupe', color: 'orange', score: 0},
+      {name: 'apple', color: 'red', score: 6},
+      {name: 'pomelo', color: 'yellow', score: 2},
+      {name: 'orange', color: 'orange', score: 9},
+      {name: 'plum', color: 'purple', score: 5}
+    ];
+  getClassForFruit(fruit: Fruit){
+    return {like: fruit.score > 7, dislike: fruit.score< 5}
+  }
 }
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+  @Input() text: string;
+  @Output() textChange = new EventEmitter<string>();
+   internalText: string = 'Angular';
+selected = 'C'
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  textChanged(event: KeyboardEvent): any {
+    const value = (event.target as any).value;
+    this.textChange.emit(value);
+  }
+}
